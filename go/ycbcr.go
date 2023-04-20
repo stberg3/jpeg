7bfddd9b6a3d9488888d5f82c6cf3ab3a39c5e11
Sam Berg <stberg3@gmail.com>
2023-04-20 15:42:31 -0500

    We got the indexing correct!

--- go/ycbcr.go
+++ go/ycbcr.go
@@ -1,122 +1,93 @@
 package main
 
 import (
-	// "encoding/base64"
-	// "fmt"
 	"image"
 	"log"
-	// "strings"
-	"io"
 	"os"
-	// Package image/jpeg is not used explicitly in the code below,
-	// but is imported for its initialization side-effect, which allows
-	// image.Decode to understand JPEG formatted images. Uncomment these
-	// two lines to also understand GIF and PNG images:
-	// _ "image/gif"
 	"image/png"
-	// "image/draw"
+	"image/jpeg"
+	"fmt"
+	// "io"
 	"image/color"
-	_ "image/jpeg"
 )
 
 
 func main() {
-	// Decode the PNG data. If reading from file, create a reader with
-	//
-	// reader, err := os.Open("media/prism.png")
-	// if err != nil {
-	//     log.Fatal(err)
-	// }
-	// defer reader.Close()
-	// m, err := png.Decode(reader)
+	
+
+	reader, err := os.Open("../media/swatch-ycbcr-8x8.jpg")
+	if err != nil {
+	    log.Fatal(err)
+	}
+	
+	defer reader.Close()	
+
+	// func Decode(r io.Reader) (image.Image, error)
+	m, err := jpeg.Decode(reader)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	processJPG(m)
+
+}
+
+func processPNG(m image.Image) {
+
+	// img = image.NewYCbCr(m.Bounds(), image.YCbCrSubsampleRatio444)
+	// fmt.Printf("The original image is a %T\n", m)
+	// fmt.Printf("The new image is a %T\n", img)
+
+	// fmt.Printf("Y: %d, Cb: %d, Cr: %d\n", len(img.Y), len(img.Cb), len(img.Cr))
+	// fmt.Printf("YStride: %d\n", img.YStride)
+	// fmt.Printf("CStride: %d\n", img.CStride)
+	// fmt.Printf("SubsampleRatio: %v\n", img.SubsampleRatio)
+	// fmt.Printf("Rect: %v\n",           img.Rect)
+
+
+	b := m.Bounds()
+	for x := b.Min.X; x < b.Max.X; x += 8 {
+		for y := b.Min.Y; y < b.Max.Y; y += 8 {		
+			fmt.Printf("(%d,%d): %v\n", x, y, m.At(x,y))
+		}
+	}
+
+	return 
+}
+
+func processJPG(m image.Image) {
+	writer, err := os.Create("../media/swatch-copy.png")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer writer.Close()
+	var img *image.YCbCr = image.NewYCbCr(m.Bounds(), image.YCbCrSubsampleRatio444)
+	b := m.Bounds()
+
+	fmt.Printf("Y Len: %d\n", len(img.Y))
+	fmt.Printf("width: %d\n", img.YStride)
+
+	for x := b.Min.X; x < b.Max.X; x++ {
+		for y := b.Min.Y; y < b.Max.Y; y++ {		
+			var r,g,b,_ uint32 = m.At(x,y).RGBA()
+			yy, cb, cr := color.RGBToYCbCr(uint8(r),uint8(g),uint8(b))
+			// yy, _, _ := color.RGBToYCbCr(uint8(r),uint8(g),uint8(b))
+			img.Y[x+y*img.YStride] = yy
+			img.Cb[x+img.CStride*y] = cb
+			img.Cr[x+img.CStride*y] = cr
+		}
+	}
+
+	// options := jpeg.Options{Quality:100}
+
+	// err = jpeg.Encode(writer, img, *options)
 	// if err != nil {
 	// 	log.Fatal(err)
 	// }
-
-	// x := m.Bounds().Max.X/2;
-	// y := m.Bounds().Max.Y/2;
-	// fmt.Printf("Pixel [0,0]: %v \n", m.At(0,0));
-	// fmt.Printf("Color [%d,%d]: %v \n", x, y, m.At(x,y));
-	// var r,g,b,_ uint32 = m.At(x,y).RGBA()
-	// var yy,cb,cr uint8 = color.RGBToYCbCr(uint8(r), uint8(g), uint8(b))
-
-	// fmt.Printf("Color [%d,%d,%d]\n", yy, cb, cr);
-	// fmt.Printf("Bounds [0,0]: %v \n", m.Bounds());
-
-	// WriteStuff("Hello! Can you hear me??")
-	// CopyStuff()
-	CopyYChannel()
-}
-
-func WriteStuff(stuff string) {
-	writer, err := os.Create("test.txt")
-
+	err = png.Encode(writer, img)
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	defer writer.Close()
-
-	if _, err := io.WriteString(writer, stuff);  err != nil {
-		log.Fatal(err)
-	}
-}
-
-func CopyStuff() {
-	reader, err := os.Open("media/prism.png")
-	if err != nil {
-	    log.Fatal(err)
-	}
-	defer reader.Close()
-
-	writer, err := os.Create("test.png")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	defer writer.Close()
-
-	if _, err := io.Copy(writer, reader); err != nil {
-		log.Fatal(err)
-	}
-
-}
-
-func CopyYChannel() {
-	reader, err := os.Open("media/prism.png")
-	if err != nil {
-	    log.Fatal(err)
-	}
-	defer reader.Close()
-	
-	m, err := png.Decode(reader)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	img := image.NewYCbCr(m.Bounds(), image.YCbCrSubsampleRatio411)
-	bounds := m.Bounds()
-
-	// for i := 0; i < img.CStride; i++ {
-		for j := 0; j < len(img.Cb); j++ {
-			var r,g,b,_ uint32 = m.At(i,j).RGBA()
-			var _,cb,_ uint8 = color.RGBToYCbCr(uint8(r), uint8(g), uint8(b))
-			// img.Y[i*bounds.Max.X+j] = yy
-			img.Cb[j] = cb
-			// img.Cr[i*bounds.Max.X+j] = cr
-		}		
-	// }
-
-
-	writer, err := os.Create("test-cb411-channel.png")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	if err := png.Encode(writer, img); err != nil {
-		log.Fatal(err)
-	}
-
-	defer writer.Close()
 
 }
